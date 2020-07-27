1ba18c77517e2e559b7e07d42b30bd5f277afda4
ipkhovelishvili <ipkhovelishvili@riotgames.com>
2020-07-27 15:46:25 +0800

    slight change in API test

--- API_test.py
+++ API_test.py
@@ -2,6 +2,6 @@ import os
 import requests
 
 os.environ['NO_PROXY'] = '127.0.0.1'
-r = requests.get('http://127.0.0.1:21337')
+r = requests.get('http://127.0.0.1:21337/positional-rectangles')
 print(r.content)
 print(r)
